f78120f321a923a9d73c3141f204e41315c09edc
Lukas Kullmann <lukas.kullmann@kiwigrid.com>
2019-02-14 10:13:43 +0100

    Add type definitions

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+export * from 'testcafe';
+import testcafe, { t } from 'testcafe';
+export default testcafe;
+
+declare module 'cucumber' {
+  export type HookFunction = (testController: typeof t) => Promise<void>;
+  export type GlobalHookFunction = (fixtureContext: { [key: string]: string }) => Promise<void>;
+  export function After(code: HookFunction): void;
+  export function After(options: string, code: HookFunction): void;
+  export function AfterAll(code: GlobalHookFunction): void;
+  export function Before(code: HookFunction): void;
+  export function Before(options: string, code: HookFunction): void;
+  export function BeforeAll(code: GlobalHookFunction): void;
+
+  export type StepFunction = (testController: typeof t, parameters: string[]) => Promise<void>;
+  export function Given(pattern: RegExp | string, code: StepFunction): void;
+  export function When(pattern: RegExp | string, code: StepFunction): void;
+  export function Then(pattern: RegExp | string, code: StepFunction): void;
+}
